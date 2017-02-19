ce5a838b316e19ade1c2c8f0a8ebbeae8dbad9cb
Stone Preston <stone.connor.preston@gmail.com>
2017-02-19 00:20:34 -0600

    added basic data display

--- index.js
+++ index.js
@@ -13,18 +13,107 @@ import {
 } from 'react-native';
 
 export default class Weathr extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tempF: null,
+      tempC: null,
+      humidity: null,
+    };
+  }
+
+  componentWillMount() {
+
+    this.getLatestData();
+
+  }
+
+  getLatestData() {
+
+    this.getLatestCelsiusTemperature();
+    this.getLatestFahrenheitTemperature();
+    this.getLatestHumidity();
+
+  }
+
+  getLatestCelsiusTemperature() {
+
+    return fetch('https://api.thingspeak.com/channels/223104/fields/1/last.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.field1);
+        this.setState({tempC: responseJson.field1});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  getLatestFahrenheitTemperature() {
+
+    return fetch('https://api.thingspeak.com/channels/223104/fields/2/last.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.field2);
+        this.setState({tempF: responseJson.field2});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  getLatestHumidity() {
+
+    return fetch('https://api.thingspeak.com/channels/223104/fields/3/last.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.field3);
+        this.setState({humidity: responseJson.field3});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+
+        {(this.state.tempC && this.state.tempF && this.state.humidity) ?
+          <Home tempF={this.state.tempF} humidity={this.state.humidity} /> 
+          :
+          <Loading /> }
+
+      </View>
+    );
+  }
+}
+
+export class Home extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Text style={styles.temperature}>
+          {this.props.tempF}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+        <Text style={styles.humidity}>
+          {this.props.humidity} % Humidity
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+
+      </View>
+    );
+  }
+
+}
+
+export class Loading extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading
         </Text>
       </View>
     );
